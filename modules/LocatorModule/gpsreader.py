4b384b33532e74fdf1b80ba5c6f5b5cbee942f0e
Feng Lu <linkcd@gmail.com>
2019-02-25 21:32:06 +0100

    update

--- modules/LocatorModule/gpsreader.py
+++ modules/LocatorModule/gpsreader.py
@@ -5,24 +5,67 @@ import json
 from datetime import datetime
 from collections import deque
 import time
+from decimal import *
 
 class GPSReader:
     class GPSDataPoint:
         def __init__(self, sentenceIimestamp):
             self.sentenceTimestamp = sentenceIimestamp
             self.data = {}
+            self.data["fixed_time"] = str(sentenceIimestamp) #make sure it is part of the json dump, have to convert from datetime to string for json dump
         
         def isFixed(self):
             if "gps_quality" in self.data and self.data["gps_quality"] == 1:
                 return True
             else:
                 return False
+
+        def getCoordiateFromValueInSentence(self, value):
+            # The latitude is formatted as DDMM.ffff and longitude is DDDMM.ffff where D is the degrees and M is minutes plus the fractional minutes. 
+            # So, 1300.8067,N is 13 degrees 00.8067 minutes North and the longitude of 07733.0003,E is read as 77 degrees 33.0003 minutes East.
+            # Converting to degrees you would have to do this: 13 + 00.8067/60 for latitude and 77 + 33.0003/60 for the longitude.
+            # ##NMEA outputs in a human readable DDDMM.mmmm format NOT DECIMAL DEGREES
+            # 3746.03837
+            # 37 46.03837
+            # 37 + (46.03837 / 60)
+            # result = 37 + 0.7673062
+            
+            #sample latitude: 5952.84746
+            #sample longitude: 01029.68131
+
+            #print("org: " + value)
+
+            segments = value.split('.')
+            if len(segments[0]) == 4:
+                #latitue
+                degree = segments[0][:2]
+            else:
+                #longtitude
+                degree = segments[0][:3]
+
+            #print("degree:" + degree)
+            #print("minute part1: " + segments[0][-2:])
+            #print("minute part2: " + segments[1])
+            
+            minute = round(Decimal(segments[0][-2:] + "." + segments[1])/60, 6)
+            
+            #print("computed minute:" + str(minute))
+            
+            result = Decimal(degree) + minute
+            
+            #print("result:" + str(result))
+
+            return str(result)
         
         def saveSentenceResult(self, result):
+            #document of protocol: https://www.gpsinformation.org/dale/nmea.htm
+            #definition of pynmea2 sentence: https://github.com/Knio/pynmea2/blob/master/pynmea2/types/talker.py
+            #sample test of pynmea2: https://github.com/Knio/pynmea2/blob/master/test/test_types.py 
+
             if result.sentence_type == 'GGA':
-                self.data["latitude"] = result.lat             
+                self.data["latitude"] = self.getCoordiateFromValueInSentence(result.lat)            
                 self.data["latitude_dir"] = result.lat_dir
-                self.data["longitude"] = result.lon
+                self.data["longitude"] = self.getCoordiateFromValueInSentence(result.lon)
                 self.data["longitude_dir"] = result.lon_dir
                 self.data["altitude"] = result.altitude
                 self.data["altitude_units"] = result.altitude_units
@@ -31,16 +74,16 @@ class GPSReader:
                 self.data["horizontal_dil"] = result.horizontal_dil
                 self.data["geo_sep"] = result.geo_sep
                 self.data["geo_sep_units"] = result.geo_sep_units
-                self.data["age_gps_data"] = result.age_gps_data
-                self.data["ref_station_id"] = result.ref_station_id
+                #self.data["age_gps_data"] = result.age_gps_data
+                #self.data["ref_station_id"] = result.ref_station_id
 
             if result.sentence_type == 'RMC':
-                self.data["datestamp"] = result.datestamp
+                self.data["fixed_date"] = str(result.datestamp) # have to convert from datetime to string for json dump
 
                 #save full timestamp (date + time)
                 if result.datestamp is not None:
                     fulldt = datetime.combine(result.datestamp, self.sentenceTimestamp)
-                    self.data["full_timestamp"] = fulldt
+                    self.data["fixed_full_timestamp"] = str(fulldt) # have to convert from datetime to string for json dump
 
                 self.data["speed"] = result.spd_over_grnd
                 self.data["true_course"] = result.true_course
@@ -60,10 +103,10 @@ class GPSReader:
     
     class GPSDataPointsManager:
         def __init__(self):
-            self.storage = deque(maxlen=5)
+            self.__storage = deque(maxlen=5)
 
         def getLatestFixedGPSPoint(self):
-            for point in self.storage:
+            for point in self.__storage:
                 if point.isFixed:
                     return point
             #return none if didnot find fixed one
@@ -71,12 +114,12 @@ class GPSReader:
 
         def __getCorrectPointToUpdate(self, sentenceTimestamp):
             #step 1. try to search and update existing one
-            for point in self.storage:
+            for point in self.__storage:
                 if point.sentenceTimestamp == sentenceTimestamp:
                     return point
             #step 2. ELSE, we cannot find the existing point with that timestamp, create new one and put it into the storage
             newPoint = GPSReader.GPSDataPoint(sentenceTimestamp)
-            self.storage.appendleft(newPoint)
+            self.__storage.appendleft(newPoint)
             return newPoint
 
         def ingestNewSentence(self, sentenceResult):
@@ -99,25 +142,26 @@ class GPSReader:
         if sentence.find('GGA') > 0:
             try:
                 result = pynmea2.parse(sentence)
-                self.pointsManager.ingestNewSentence(result)
-            except:
-                print("drop invalid GGA: " + sentence)
+                self.__pointsManager.ingestNewSentence(result)
+            except Exception as e:
+                print("drop invalid GGA: " + sentence + ". Error: " + str(e))
 
         if sentence.find('RMC') > 0:
             try:
                 result = pynmea2.parse(sentence)
-                self.pointsManager.ingestNewSentence(result)
-            except:
-                print("drop invalid RMC: " + sentence)
+                self.__pointsManager.ingestNewSentence(result)
+            except Exception as e:
+                print("drop invalid RMC: " + sentence + ". Error: " + str(e))
 
     def __initSerialStream(self):
         print(">>> Initaling GPS serial stream from: " + self.__serialttyAC)
         self.isFixed = False
 
         if self.__serialStream is not None and not self.__serialStream.closed:
+            print("close serial stream...")
             self.__serialStream.close()
 
-        self.serialStream = serial.Serial(self.__serialttyAC, 9600, timeout=0.5)
+        self.__serialStream = serial.Serial(self.__serialttyAC, 9600, timeout=0.5)
 
     # Gps Receiver thread funcion, check gps value for infinte times
     def __readSerialData(self):
@@ -127,7 +171,7 @@ class GPSReader:
                 self.__initSerialStream()
                 try:
                     while True:
-                        sentence = self.serialStream.readline()
+                        sentence = self.__serialStream.readline()
                         self.__parseGPSData(sentence)
                 except Exception as e:
                     print("Error: " + str(e))        
--- modules/LocatorModule/test-gps.py
+++ modules/LocatorModule/test-gps.py
@@ -2,7 +2,7 @@
 from gpsreader import GPSReader 
 import time
 import json
-
+from datetime import datetime
 
 #start reading gps data...
 myGPSReader = GPSReader("/dev/ttyACM0")
@@ -10,11 +10,11 @@ latestFixedTime = None
 
 while True:
     latestFixedPoint = myGPSReader.getLatestFixedGPSPoint()
-    if latestFixedPoint is not None and latestFixedPoint.sentenceIimestamp != latestFixedTime:
+    if latestFixedPoint is not None and latestFixedPoint.sentenceTimestamp != latestFixedTime:
         #new GPS point than previous one, send to iot hub
         jsonPayload = latestFixedPoint.buildJsonPayload("test_device")
         print("Payload of new GPS data is: " + jsonPayload)
         print("++++++++++++++++++++")
     else:
-        print("no fixed yet...")
+        print(str(datetime.now()) + ": Not fixed yet....")
     time.sleep(1)
